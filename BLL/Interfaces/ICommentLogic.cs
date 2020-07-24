8a48cf90787619b1cbd0bcba8307c4488a01eddd
USER <haidoan9205@gmail.com>
2020-07-24 14:46:35 +0700

    comment logic

---
+++ BLL/Interfaces/ICommentLogic.cs
@@ -0,0 +1,16 @@
+﻿using BLL.BusinessLogics;
+using BLL.Models.CommentModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface ICommentLogic
+    {
+        public bool CreateNewComment(CommentModel comment);
+        public bool DeleteComment(int id);
+        public bool UpdateComment(CommentLogic comment);
+        public CommentLogic GetCommentById(int id);
+    }
+}
